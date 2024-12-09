9ae05c4e75dfc634bdaae644941bec0ff06b0d84
IsabeleSilvestre <isabele.silvestre.27@gmail.com>
2024-12-09 18:26:05 -0300

    atualizacao

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from modelos.restaurante import Restaurante
+from restaurante import Restaurante
 
 restaurante_praca = Restaurante('Almeida', 'Self-service')
 restaurante_praca.receber_avaliacao('Augusto', 5)
 restaurante_praca.receber_avaliacao('Eduarda', 1)
-restaurante_praca.receber_avaliacao('Mariana', 7)
+restaurante_praca.receber_avaliacao('Mariana', 4)
 
 restaurante_praca = Restaurante('Altas', 'Gourmet')
-restaurante_praca.receber_avaliacao('Elvis', 6)
+restaurante_praca.receber_avaliacao('Elvis', 3)
 restaurante_praca.receber_avaliacao('Amanda', 5)
 restaurante_praca.receber_avaliacao('Naldo', 5)
 
